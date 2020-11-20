bb515519b29a781f48fb16ae07e354a5520106dc
Philipp Veit <philipp.veit@devk.de>
2020-11-20 15:45:42 +0100

    Mehr Funktionen und besseres Aussehen

--- src/main/java/com/example/monitoringServer/Controller/MonitoringServerController.java
+++ src/main/java/com/example/monitoringServer/Controller/MonitoringServerController.java
@@ -1,17 +1,23 @@
 package com.example.monitoringServer.Controller;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import com.example.monitoringServer.Model.CamundaObject;
 import com.example.monitoringServer.Service.CamundaObjectService;
 import org.thymeleaf.model.IModel;
+
+import static com.example.monitoringServer.Model.CamundaObject.CamundaObjectBuilder.aCamundaObject;
+import static java.util.Comparator.comparingLong;
 
 @Controller
 //@RequestMapping("/camundaObject")
@@ -25,34 +31,33 @@ public class MonitoringServerController {
     // nichts senden
     @GetMapping("/")
     public String getCamundaObject(Model model) {
-        try {
-            List<CamundaObject> allcamundaObjects = new ArrayList();
-            allcamundaObjects = camundaObjectService.getCamundaObjectsList();
-            model.addAttribute("tolle_liste", allcamundaObjects);
-            logger.info("gibt die Liste von CamundaObjects zurück");
-            return "CamundaObjectList";
-        }catch (Exception e){
-            logger.info(e.getMessage());
-            return "index";
-        }
+        List<CamundaObject> completeList = camundaObjectService.getCamundaObjectsList();
+        model.addAttribute("tolle_liste", completeList);
+        CamundaObject max = completeList.stream().max(comparingLong(CamundaObject::getDurchlaufzeit_nano)).orElse(aCamundaObject().build());
+        model.addAttribute("maxObject", max.getDurchlaufzeit_sec());
+        CamundaObject min = completeList.stream().min(comparingLong(CamundaObject::getDurchlaufzeit_nano)).orElse(aCamundaObject().build());
+        model.addAttribute("minObject", min.getDurchlaufzeit_sec());
+        model.addAttribute("averageObject_nano", completeList.stream().mapToLong(CamundaObject::getDurchlaufzeit_nano).average().orElse(0));
+        model.addAttribute("averageObject_ms", completeList.stream().mapToLong(CamundaObject::getDurchlaufzeit_ms).average().orElse(0));
+        model.addAttribute("averageObject_s", completeList.stream().mapToLong(CamundaObject::getDurchlaufzeit_sec).average().orElse(0));
+        model.addAttribute("amountObjects", completeList.size());
+        logger.info("gibt die Liste von CamundaObjects zurück");
+        return "CamundaObjectList";
+
     }
+
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
     }
+
     //camunda object an den service übergeben , ziel: im server speichern, attribute müssen auch hier stehen, gateway einbauen,
-    @PostMapping("/")
-    public void createCamundaObject(@RequestBody CamundaObject camundaObject ) {
+    @PostMapping("/monitoring")
+    @ResponseStatus(value = HttpStatus.OK)
+    public void createCamundaObject(@RequestBody CamundaObject camundaObject) {
         logger.info(camundaObject.toString());
-
-        CamundaObject camundaObject1 = new CamundaObject();
-
-        camundaObject1.setStartzeit(camundaObject.getStartzeit());
-        camundaObject1.setEndzeit(camundaObject.getEndzeit());
-        camundaObject1.setDurchlaufzeit(camundaObject.getDurchlaufzeit());
-
-        camundaObjectService.saveCamundaObject(camundaObject1);
+        camundaObjectService.saveCamundaObject(camundaObject);
     }
 }
 
--- src/main/java/com/example/monitoringServer/Model/CamundaObject.java
+++ src/main/java/com/example/monitoringServer/Model/CamundaObject.java
@@ -1,66 +1,128 @@
 package com.example.monitoringServer.Model;
 
-import java.io.Serializable;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonSerialize
-public class CamundaObject implements Serializable{
+public class CamundaObject implements Serializable {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long camundaObject_id;
-	private long startzeit;
-	private long endzeit;
-	private long durchlaufzeit;
-	
-	public CamundaObject() {
-		
-	}
-	
-	public CamundaObject(long startzeit, long endzeit, long durchlaufzeit) {
-		this.durchlaufzeit = durchlaufzeit;
-		this.endzeit = endzeit;
-		this.endzeit = startzeit;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long camundaObject_id;
+    private long startzeit;
+    private long endzeit;
+    private long durchlaufzeit_nano;
+    private long durchlaufzeit_ms;
+    private long durchlaufzeit_sec;
 
-	public long getStartzeit() {
-		return startzeit;
-	}
+    public long getCamundaObject_id() {
+        return camundaObject_id;
+    }
 
-	public void setStartzeit(long startzeit) { this.startzeit = startzeit; }
+    public long getStartzeit() {
+        return startzeit;
+    }
 
-	public long getEndzeit() {
-		return endzeit;
-	}
+    public long getEndzeit() {
+        return endzeit;
+    }
 
-	public void setEndzeit(long endzeit) {
-		this.endzeit = endzeit;
-	}
+    public long getDurchlaufzeit_nano() {
+        return durchlaufzeit_nano;
+    }
 
-	public long getDurchlaufzeit() {
-		return durchlaufzeit;
-	}
+    public long getDurchlaufzeit_ms() {
+        return durchlaufzeit_ms;
+    }
 
-	public void setDurchlaufzeit(long durchlaufzeit) {
-		this.durchlaufzeit = durchlaufzeit;
-	}
+    public long getDurchlaufzeit_sec() {
+        return durchlaufzeit_sec;
+    }
 
-	@Override
-	public String toString() {
-		return "CamundaObject{" +
-				"startzeit=" + startzeit +
-				", endzeit=" + endzeit +
-				", durchlaufzeit=" + durchlaufzeit +
-				'}';
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CamundaObject that = (CamundaObject) o;
+        return startzeit == that.startzeit &&
+                endzeit == that.endzeit &&
+                durchlaufzeit_nano == that.durchlaufzeit_nano &&
+                durchlaufzeit_ms == that.durchlaufzeit_ms &&
+                durchlaufzeit_sec == that.durchlaufzeit_sec;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(startzeit, endzeit, durchlaufzeit_nano, durchlaufzeit_ms, durchlaufzeit_sec);
+    }
+
+    @Override
+    public String toString() {
+        return "CamundaObject{" +
+                "startzeit=" + startzeit +
+                ", endzeit=" + endzeit +
+                ", durchlaufzeit_nano=" + durchlaufzeit_nano +
+                ", durchlaufzeit_ms=" + durchlaufzeit_ms +
+                ", durchlaufzeit_sec=" + durchlaufzeit_sec +
+                '}';
+    }
+
+    public static final class CamundaObjectBuilder {
+        private long startzeit;
+        private long endzeit;
+        private long durchlaufzeit_nano;
+        private long durchlaufzeit_ms;
+        private long durchlaufzeit_sec;
+
+        private CamundaObjectBuilder() {
+        }
+
+        public static CamundaObjectBuilder aCamundaObject() {
+            return new CamundaObjectBuilder();
+        }
+
+        public CamundaObjectBuilder withStartzeit(long startzeit) {
+            this.startzeit = startzeit;
+            return this;
+        }
+
+        public CamundaObjectBuilder withEndzeit(long endzeit) {
+            this.endzeit = endzeit;
+            return this;
+        }
+
+        public CamundaObjectBuilder withDurchlaufzeit_nano(long durchlaufzeit_nano) {
+            this.durchlaufzeit_nano = durchlaufzeit_nano;
+            return this;
+        }
+
+        public CamundaObjectBuilder withDurchlaufzeit_ms(long durchlaufzeit_ms) {
+            this.durchlaufzeit_ms = durchlaufzeit_ms;
+            return this;
+        }
+
+        public CamundaObjectBuilder withDurchlaufzeit_sec(long durchlaufzeit_sec) {
+            this.durchlaufzeit_sec = durchlaufzeit_sec;
+            return this;
+        }
+
+        public CamundaObject build() {
+            CamundaObject camundaObject = new CamundaObject();
+            camundaObject.durchlaufzeit_ms = this.durchlaufzeit_ms;
+            camundaObject.durchlaufzeit_sec = this.durchlaufzeit_sec;
+            camundaObject.durchlaufzeit_nano = this.durchlaufzeit_nano;
+            camundaObject.startzeit = this.startzeit;
+            camundaObject.endzeit = this.endzeit;
+            return camundaObject;
+        }
+    }
 }
