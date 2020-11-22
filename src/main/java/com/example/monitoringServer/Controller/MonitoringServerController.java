b66ab5490a81342ebaf3ba482d89aa7c18760956
jiyebi <iyebi.jojo@gmail.com>
2020-11-22 15:33:59 +0100

    neue Sachen in Thymleaf probieren

--- src/main/java/com/example/monitoringServer/Controller/MonitoringServerController.java
+++ src/main/java/com/example/monitoringServer/Controller/MonitoringServerController.java
@@ -14,7 +14,7 @@ import com.example.monitoringServer.Service.CamundaObjectService;
 import org.thymeleaf.model.IModel;
 
 @Controller
-//@RequestMapping("/camundaObject")
+@RequestMapping("/camundaObject")
 public class MonitoringServerController {
 
     private static final Logger logger = LoggerFactory.getLogger(MonitoringServerController.class);
@@ -28,12 +28,12 @@ public class MonitoringServerController {
         try {
             List<CamundaObject> allcamundaObjects = new ArrayList();
             allcamundaObjects = camundaObjectService.getCamundaObjectsList();
-            model.addAttribute("tolle_liste", allcamundaObjects);
+            model.addAttribute("camundaObjectsList", allcamundaObjects);
             logger.info("gibt die Liste von CamundaObjects zurück");
             return "CamundaObjectList";
         }catch (Exception e){
             logger.info(e.getMessage());
-            return "index";
+            return "error";
         }
     }
     @GetMapping("/greeting")
@@ -43,11 +43,10 @@ public class MonitoringServerController {
     }
     //camunda object an den service übergeben , ziel: im server speichern, attribute müssen auch hier stehen, gateway einbauen,
     @PostMapping("/")
-    public void createCamundaObject(@RequestBody CamundaObject camundaObject ) {
+    public void createCamundaObject( @RequestBody CamundaObject camundaObject ) {
         logger.info(camundaObject.toString());
 
         CamundaObject camundaObject1 = new CamundaObject();
-
         camundaObject1.setStartzeit(camundaObject.getStartzeit());
         camundaObject1.setEndzeit(camundaObject.getEndzeit());
         camundaObject1.setDurchlaufzeit(camundaObject.getDurchlaufzeit());
